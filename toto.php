c563d5961f63731575171d56ca120afda84bae10
karimm <karimm991@gmail.com>
2021-11-09 12:01:55 +0100

    toto blague

--- toto.php
+++ toto.php
@@ -1,4 +1,4 @@
 <?php
-print( " c'est l'histoire de toto qui va à l'école !  <br>");
+print( " C'est l'histoire de Toto qui fait de la luge avec sa frangine. Leur mère lui demande « Dis-moi Toto chéri, penses-tu à prêter ta luge à ta sœur ? ». Toto lui répond « Oui maman, moi je l'ai pour descendre et elle l'a pour monter ». Hahaha quel gros bâtard cet enfoiré de Toto  <br>");
 
 ?>
