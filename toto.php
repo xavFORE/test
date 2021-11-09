43b086ff1731fad558c8544c018608ff675e4988
karimm <karimm991@gmail.com>
2021-11-09 11:44:04 +0100

    c'est l'histoire toto

--- toto.php
+++ toto.php
@@ -1,4 +1,4 @@
 <?php
-print( " c'est un gars qui va voir sa femme et qui lui dit  chérie ce soir tu me vois  pas !  <br> elle pourquoi tu sors <br> non, je t'encule lol  <br>");
+print( " c'est l'histoire de toto qui va à l'école !  <br>");
 
 ?>
