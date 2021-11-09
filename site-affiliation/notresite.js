0732128519bb446b0aa3235b7fb1fc37a0700a60
bouchra <bouchra.saillier.foreach@gmail.com>
2021-11-09 15:09:48 +0100

    change de tire

--- site-affiliation/notresite.js
+++
@@ -1,6 +0,0 @@
-
-let d1 = document.getElementById("d1");
-let p1 = document.getElementById("p1");
-
-d1.addEventListener("mouseover", () => {p1.style.display = "block";});
-d1.addEventListener("mouseout", () => {p1.style.display = "none";});
