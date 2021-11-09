e329c8bd154c65b90faf1b3660bc2a1e258fcf91
xavier <xavier.bourget@foreach-academy.fr>
2021-11-09 12:43:49 +0100

    toto a la peche

--- ballons/maLibPerso.js
+++ ballons/maLibPerso.js
@@ -18,8 +18,9 @@ function rndIntervalleInt(min, max)
 {
     return Math.floor(rndIntervalleFloat(min, max ));
 }
-function rndIntervalleFloat(min, max) {
-    return Math.random() * (max - min) + min;
 
+function rndIntervalleFloat(min, max) 
+{
+    return Math.random() * (max - min) + min;
 }
 
