4f60c6c67e9333f85d67a4c05c02ae69109ac646
MehdiForeach <mehdi.birembaut.foreach@gmail.com>
2021-11-10 17:34:35 +0100

    maj

--- form01Mfiltre.php
+++ form01Mfiltre.php
@@ -1,7 +1,30 @@
 <?php
+
+$users = ["xavier","olivier","remi","alexis","nelly","mehdi","hanane","bouchra","karim"];
+
     if ( $_POST )
     {
         $nom = $_POST[ "nom" ];
+
+            if ($nom == "xavier")
+                print($nom . " -> " . "vieux");
+            else if ($nom == "olivier")
+                print($nom . " -> " . "voyage");
+            else if ($nom == "remi")
+                print($nom . " -> " . "train");
+            else if ($nom == "alexis")
+                print($nom . " -> " . "voiture");
+            else if ($nom == "nelly")
+                print($nom . " -> " . "rage");
+            else if ($nom == "mehdi")
+                print($nom . " -> " . "ange");
+            else if ($nom == "hanane")
+                print($nom . " -> " . "argan");
+            else if ($nom == "bouchra")
+                print($nom . " -> " . "brownie");
+            else if ($nom == "karim")
+                print($nom . " -> " . "javascript");
+            else print($nom . " qui est tu ?");
         
         // si xavier  -> vieux
         // si olivier  -> voyage
@@ -16,9 +39,6 @@
         exit();
     }
 ?>
-
-
-
 
 <form action="#" method="post">
     <input type="text" name="nom" placeholder="ton nom">
