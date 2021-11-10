7ed01cfad274d6b53e0f083c621ca4841d0a5323
xavier <xavier.bourget@foreach-academy.fr>
2021-11-10 10:11:07 +0100

    mon premier PHP

---
+++ base01.php
@@ -0,0 +1,22 @@
+
+<h1>Hello le PHP</h1>
+
+<?php
+
+    $nom = "toto";
+
+    for( $i=0 ; $i<3 ; $i++ ) 
+        print( "<h1>Hello $nom    ligne:$i</h1>\n" );
+
+
+
+        disBonjour( "Olivier" );    
+        disBonjour( "Karim" );  
+        
+        
+    function disBonjour( $nom )
+    {
+        print( "bonjour $nom<br>\n" );
+    }
+
+?>
