ef673f72ce346decb3857d98ecca1843a30ebc50
xavier <xavier.bourget@foreach-academy.fr>
2021-11-10 17:08:31 +0100

    form filtre

--- dictionnaire01X.php
+++ dictionnaire01X.php
@@ -13,11 +13,10 @@
     $magasin = [ "couscous" => 17, "tomates" => 4.3, "navet" => 5, "carrotes" => 6, "pois chiche" => 1.4 ];
     
 
+
     
     
-    
- 
-    print( $dictionnaire[ music ]."<br>" ) ;
+    print( $dictionnaire[ "music" ]."<br>" ) ;
 
 
 
