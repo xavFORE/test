08d48684bee0cbcc675b39110a6354a9e56e1b56
MehdiForeach <mehdi.birembaut.foreach@gmail.com>
2021-11-12 12:45:04 +0100

    maj

--- form01Mfiltre.php
+++ form01Mfiltre.php
@@ -8,20 +8,32 @@
             "alexis"    => " achète des voitures",
             "nelly"     => " est en rage",
             "mehdi"     => " est un ange",
-            "hanane"    => " utilise de l'argan",
+            "hanane"    => " utulise de argan",
             "bouchra"   => " aime les brownies",
             "karim"     => " est un crack en javascript"
         ];
 
+        // XaviER
         $nom = $_POST[ "nom" ];
-        
-        $mes = $prenoms[ strtolower($nom)];
 
-        print( $mes."<br>" );
+        // XaviER -> xavier
+        $nom = strtolower( $nom  );
+        
+        if ( array_key_exists( $nom, $prenoms ) )
+        {
+            $mes = $prenoms[ $nom ];
+            print( $nom." ".$mes."<br>" );
+        }
+        else
+        {
+            array_push($prenoms, $nom);
+            $mes = $prenoms[ $nom ];
+            print( $nom." "." est ajouté"."<br>" );
+        }
+        
+        //exit();
     }
 ?>
-
-
 
 
 <form action="#" method="post">
