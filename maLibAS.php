9ac2e10c7f1b87518eb16fc9f1ebd5b61df63478
Santrain <alexis.santrain@orange.fr>
2021-11-15 16:16:42 +0100

    15/11 16h16

--- maLibAS.php
+++ maLibAS.php
@@ -1,6 +1,9 @@
 <?php
-function printSL ($mes){
-    print($mes."\n");
+
+// print Saut de Ligne
+function printSL( $mes )
+{
+    print( $mes."\n" );
 }
 
 function entete( $titre )
@@ -12,28 +15,36 @@ function entete( $titre )
     printSL( '    <meta http-equiv="X-UA-Compatible" content="IE=edge">');
     printSL( '    <meta name="viewport" content="width=device-width, initial-scale=1.0">');
     printSL( "    <title>$titre</title>");
-    printSL( '<link rel="stylesheet" href="monCssAS.css">');
+    printSL( '<link rel="stylesheet" href="monCSSAS.css">');
     printSL( '</head>');
     printSL( '<body>');
-}  
-
-function ecritEnGrand( $text, $css="" )
-{
-    if($css){
-    printSL('<h1 class="'.$css.'">'.strtoupper($text)."</h1>");
-    }
-    else
-    printSL('<h1>'.strtoupper($text)."</h1>");
-    
-}
-
+}   
 
 function pied()
 {
     printSL( '</body>');
     printSL( '</html>');
 }
+         
+function ecritEnGrand( $mes, $class="" )
+{
+    if ( $class != "" )
+        $class = " class='$class'";
 
+    $mes = strtoupper( $mes );
+    printSL( "<h1$class>$mes</h1>" );
+    //printSL( '<h1 class="'.$class.'">'.$mes.'</h1>' );
+    // <h1 class="enGrand">bonjour TOTO !!</h1>
+    // <h1 class="enMoyen">bonjour TOTO !!</h1>
+}
+function lienVers($infos, $lien){
+    $lienherf = 'href="'.$lien.'"';
+    printSL("<a ".$lienherf.">".$infos."</a>");
+}
 
-
-?>
+/*
+sauver le travail courant -> 
+git status
+git add ........
+git commit
+*/
---
+++ mesinfosAS.php
@@ -0,0 +1,15 @@
+<?php
+require "maLibAS.php";
+
+
+// ma page commence ici
+
+entete( "mes infos");
+
+ecritEnGrand( "le magasin est ouvert de 11:55 à midi ", "enMoyen" );
+
+lienVers("retour vers l'accueil","bonjourToto01AS.php");
+
+//print( '<h1>bonjour TOTO !!</h1>');
+pied();
+?>
