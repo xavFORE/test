a74a13b65182cdd8efad2a1d5d69ea8a759f7a6d
remi <remi.verhaeghe.foreach@gmail.com>
2021-11-15 11:44:43 +0100

    modif

---
+++ bonjourToto01RV.php
@@ -0,0 +1,11 @@
+<?php
+require "maLibRV.php";
+
+
+// ma page commence ici
+
+entete( "salut");
+ecritEnGrand( "bonjour TOTO !!" );
+//print( '<h1>bonjour TOTO !!</h1>');
+pied();
+?>
---
+++ maLibRV.php
@@ -0,0 +1,35 @@
+<?php
+
+// print Saut de Ligne 
+function printSL($mess){
+    print($mess )
+}
+
+
+function entete( $titre )
+{
+    print( '<!DOCTYPE html>');
+    print( '<html lang="fr">');
+    print( '<head>');
+    print( '    <meta charset="UTF-8">');
+    print( '    <meta http-equiv="X-UA-Compatible" content="IE=edge">');
+    print( '    <meta name="viewport" content="width=device-width, initial-scale=1.0">');
+    print( "    <title>$titre</title>");
+    print( '</head>');
+    print( '<body>');
+}   
+
+function pied()
+{
+    print( '</body>');
+    print( '</html>');
+}
+
+function ecritEnGrand($phrase)
+{
+    print( "<h1>$phrase</h1>");
+}
+
+
+
+?>
