0ddeee7be8e6caf08dd5afec28d316ddcf5aac11
karimm <karimm991@gmail.com>
2021-11-15 11:46:44 +0100

    Lundi2

--- bonjourToto01Km.php
+++ bonjourToto01Km.php
@@ -5,7 +5,9 @@ require "maLibKm.php";
 // ma page commence ici
 
 entete( "salut");
+ecritEnGrand( "Ceci à la taille d'un titre grace à la balise h1 !!" );
 ecritEnGrand( "bonjour TOTO !!" );
+ecritEnGrand( "nous sommes lundi  !!" );
 //print( '<h1>bonjour TOTO !!</h1>');
 pied();
 ?>
