7ca1240159194bc3b41d32724a417997dfb715e5
carliolivier <olivier.carlier.foreach@hotmail.com>
2021-11-15 09:56:37 +0100

    dossier

---
+++ function/faireDuPain.php
@@ -0,0 +1,29 @@
+<?php
+
+
+function faireDuPain() 
+{
+print ("je veut du pain")
+};
+mettreDeLaFarine ()
+{
+    print ("je met de la farine !") 
+};
+mettreDeLeau ()
+{
+    print ("je met de l'eau")
+};
+mettreDeLaLevure ()
+{
+    print ("je met de la levure")
+};
+
+
+
+
+
+
+
+
+
+?>
