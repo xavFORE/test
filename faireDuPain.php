df5c10ca58ab6870c87e14e92722cf50b03b2335
Nelly <nelly.descamps.foreach@gmail.com>
2021-11-15 10:36:31 +0100

    maj

---
+++ faireDuPain.php
@@ -0,0 +1,48 @@
+<?php
+
+            faireDuPain();
+
+    function faireDuPain()
+    {
+        preparationDesIngredients();
+        melangerLesIngredients();
+        faireLeverLaPate();
+        decouperLesPatons();
+        cuireLePain();
+    }
+
+
+    function preparationDesIngredients()
+    {
+        print("tamiser La Farine");
+        print("preparer La Levure");
+    }
+
+    
+    function melangerLesIngredients()
+    {
+        print("Utiliser le robot mélangeur ");
+        print("appuyer Sur Le Bouton ON");
+
+    }
+
+    function faireLeverLaPate()
+    {
+        print("Utiliser la chambre chaude");
+        //verifierLaTemperature();
+        print("placer LaPate Au Chaud");
+        print("mettre Le Timer");
+    }
+    function decouperLesPatons()
+    {
+        print("Découper les patons");
+    }
+    function cuireLePain()
+    {
+        print("placer les patons dans le four ");
+    }
+
+        
+    
+?>
+
