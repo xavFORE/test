8199c435742a794a827640edcdb91edb1783a825
xavier <xavier.bourget@foreach-academy.fr>
2021-11-16 13:50:51 +0100

    init panier

---
+++ monpanierX.php
@@ -0,0 +1,26 @@
+<?php
+
+$monPanier = 
+[
+    [ "fraise"      ,     2.5,      5 ],
+    [ "framboise"   ,     1.5,      5 ],
+    [ "sardine"     ,     0.2,    4.5 ],
+    [ "sucre"       ,       7,      1 ],
+    [ "PQ"          ,     100,    0.5 ],
+    [ "mitraillete" ,       9,    999 ],
+    [ "chaussettes" ,       3,     15 ]
+];
+
+
+print( $monPanier[1][1]  );
+
+
+
+
+
+
+
+
+
+
+?>
