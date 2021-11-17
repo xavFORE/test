c8db6d8e35d2141d2e90609de8041850e90287af
Santrain <alexis.santrain@orange.fr>
2021-11-17 15:26:38 +0100

    k

---
+++ objGeo01CarreRectAS.php
@@ -0,0 +1,18 @@
+<?php
+
+require "maLibGeoAS.php";
+
+// ********************************************************
+
+// Carre( x, y, cote );
+$k1 = new Carre( 10, 15, 100 );
+// Rectangle( x, y, largeur, longueur );
+$r1 = new Rectangle( 11, 13, 100, 200 );
+
+$k1->aff();
+//je suis un carré en (12,23) de coté 10
+
+$r1->aff();
+//je suis un rectangle en (11,13) de largeur 100, de hauteur 200
+
+?>
