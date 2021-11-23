06fdb2a52bae2229b0aebe2e3367db3d88bf0469
unknown <hanane.alaoui.foreach@gmail.com>
2021-11-23 16:03:47 +0100

    oo

--- geoH.php
+++ geoH.php
@@ -4,45 +4,55 @@
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-  
     <title>Document</title>
     <style>
         .figure
         {
             position: absolute;
-        
         }
-        .foto
+        .fototadapt
         {
-        
-          width : 100%;
-          height: 100%;
+            width:100%;
+            height:100%;
         }
-    
 
     </style>
 </head>
 <body>
 
-<a href="https://www.govoyages.com/"><img src="images/av5.png" style="white-space: inherit;"></a>
+
 <?php
 
 require_once "geoLibH.php";
 
 
-$p1 = new Point( 12, 5 );                          // creation Point 12,5
-$c1 = new Carre( 150, 150, 200, "red");       // creation Carré 3, 15, 100
-$c2 = new Carre( 30, 300, 100, "green");
-$c3 = new Carre( 150, 400, 150, "blue");
-$p = new Page();                            // creation de la page
-$f1 = new foto( 150, 150, 200, "pink","images/av5.png");
-$t1 = new Text( 10, 200, 60, "red", "bonjour toto");
-$l1 = new lien( 10, 150, 100,"https://www.govoyages.com/");
+$p1 = new Point( 12, 5 );
+// creation Point 12,5
+$c1 = new Carre( 3, 15, 100, "yellow");
+// creation Carré 3, 15, 100
+
+$c2 = new Carre( -3, -15, 50, "black");
+$c3 = new Carre( 30, 150, 10, "orange");
+
+$t1 = new Text( 150, 200, 50, "red", "Hello Toto" );
+$t2 = new Text( 100, 100, 50, "red", '<img class="fototadapt" src="images/av1.jpeg">' );
+$f1 = new Foto( 150, 200, 50, "images/av1.jpeg" );
+
+$l1 = new Lien( 250, 200, 50, "red", "geoH.php" );
+
+$c1->addContent( "bonjour Toto");
+//$t2->move( 20, 0);
+
+
+$p = new Page();
+// creation de la page
+
 
 $p->addFigure( $c1 );
 $p->addFigure( $c2 );
 $p->addFigure( $c3 );
 $p->addFigure( $t1 );
+$p->addFigure( $t2 );
 $p->addFigure( $f1 );
 $p->addFigure( $l1 );
 
