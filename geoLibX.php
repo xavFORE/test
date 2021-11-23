69343cc317e6594df8b9d7b54c92e3c4202d7a6e
xavier <xavier.bourget@foreach-academy.fr>
2021-11-23 10:26:11 +0100

    lib geo

--- geoLibX.php
+++ geoLibX.php
@@ -53,25 +53,4 @@ class Page
     }
 }
 
-
-
-
-$p1 = new Point( 12, 5 );
-// creation Point 12,5
-$c1 = new Carre( 3, 15, 100, "yellow");
-// creation Carré 3, 15, 100
-
-$c2 = new Carre( -3, -15, 50, "black");
-$c3 = new Carre( 30, 150, 10, "orange");
-
-$p = new Page();
-// creation de la page
-
-
-$p->addFigure( $c1 );
-$p->addFigure( $c2 );
-$p->addFigure( $c3 );
-
-$p->affiche();
-
 ?>
--- geoX.php
+++ geoX.php
@@ -28,6 +28,10 @@ $c1 = new Carre( 3, 15, 100, "yellow");
 $c2 = new Carre( -3, -15, 50, "black");
 $c3 = new Carre( 30, 150, 10, "orange");
 
+$t1 = new Text( 150, 200, 50, "Hello Toto" );
+
+$f1 = new Photo( 150, 200, 50, "images/av1.jpeg" );
+
 $p = new Page();
 // creation de la page
 
@@ -35,6 +39,7 @@ $p = new Page();
 $p->addFigure( $c1 );
 $p->addFigure( $c2 );
 $p->addFigure( $c3 );
+$p->addFigure( $f1 );
 
 $p->affiche();
 
