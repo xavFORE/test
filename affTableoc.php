840dc2c7224c721169dfdb9d3b9d09ad37ad4d88
carliolivier <olivier.carlier.foreach@hotmail.com>
2021-11-25 14:42:41 +0100

    giti

---
+++ affTableoc.php
@@ -0,0 +1,29 @@
+<?php
+    // creer un table nom et pw
+
+        // connecter à votre DB
+require_once "ressources.php";
+        $mysqli = new mysqli($servername, $username, $password, $database);
+
+        // forger la requete
+        $query  = "select * from form1;";
+        
+        // execute la requete
+        $res = $mysqli->query( $query );
+
+        print( "<table>\n");
+        while(  ($ligne = $res->fetch_assoc()) )
+        {
+            print( "<tr>\n" );
+            foreach( $ligne as $key => $values )
+            {
+                print("<td>\n" );
+                    print( $values  );
+                print("</td>\n" );
+            }
+            print( "</tr>\n" );
+        }
+        print( "</table>\n");
+
+        //fermer la DB
+       $mysqli->close();
