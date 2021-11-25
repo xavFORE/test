dc6b96ff3838595512d0d0c62a3d8a75014df843
MehdiForeach <mehdi.birembaut.foreach@gmail.com>
2021-11-25 17:11:32 +0100

    maj

--- loginM.php
+++ loginM.php
@@ -1,9 +1,11 @@
 <?php
-
     require_once "ressources.php";
 
     if ($_GET)
     {
+        $nom = $_GET[ "nom" ];
+        $pw  = $_GET[ "pw" ];
+
         $mysqli = new mysqli($servername, $username, $password, $dbname);
 
         if ($mysqli -> connect_error) {
@@ -13,13 +15,38 @@
     
         $query = "SELECT nom FROM users";
         $resultat = $mysqli->query($query);
-    
+
         $ligne = $resultat->fetch_assoc();
-        $moy = $ligne["moy"];
-        print("Moyenne des âges : $moy");
+
+        foreach( $ligne as $key => $values )
+        {
+            if($nom == $values)
+                print("Ok $nom tu es bien dans la DB !");
+            else
+                print("Qui es tu $nom ?");
+        }
 
         $mysqli->close();
     }
-
 ?>
 
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    
+<form action="#" method="GET">
+    <input type="text" placeholder="saisir login" name="nom">
+    <br>
+    <input type="password" placeholder="saisir PW" name="pw">
+    <br>
+    <button type="submit">CONNECT</button>
+</form>
+
+</body>
+</html>
