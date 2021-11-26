4e8ca5d2293cb6f63a664995378479d62457d4e2
unknown <hanane.alaoui.foreach@gmail.com>
2021-11-26 12:20:33 +0100

    ttt

---
+++ loginH.php
@@ -0,0 +1,57 @@
+
+<?php
+    // creer un table nom et pw
+    require_once "ressources.php";
+
+    if ( $_GET )
+    {
+        $nom = $_GET[ "nom" ];
+        $pw  = $_GET[ "pw" ];
+        //print( "la valeur du champ : $maValeur<br> ");
+        // connecter à votre DB
+        $mysqli = new mysqli($servername, $username, $password, $database);
+        // forger la requete
+        $query  = " select id from clients where nom='$nom' and pwd='$pw';";
+        $ret = $mysqli->query( $query );
+        $ligne = $ret->fetch_assoc();
+        if($ligne["id"]==3)
+        print("conexion");
+        else
+        print("invalide");
+        
+
+
+        // un print bien utile pour débugger
+        //print( $query );
+
+        // execute la requete
+        //$ret = $mysqli->query( $query );
+        //print_r($ret);
+        //fermer la DB
+        $mysqli->close();
+    }
+  
+?>
+
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    
+<form action="#" method="GET">
+    <input type="text" placeholder="saisir login" name="nom" >
+    <br>
+    <input type="text" placeholder="saisir PW" name="pw" >
+    <br>
+    <button type="submit">CONNECT</button>
+</form>
+
+
+</body>
+</html>
