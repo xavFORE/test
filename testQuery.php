7cae9e84031dea489d1a4256ab0229e468a71fa3
xavier <xavier.bourget@foreach-academy.fr>
2021-11-26 12:21:41 +0100

    mysql login

---
+++ testQuery.php
@@ -0,0 +1,40 @@
+
+<?php
+    // creer un table nom et pw
+    require_once "ressources.php";
+
+        // connecter à votre DB
+        $mysqli = new mysqli($servername, $username, $password, $database);
+
+        // forger la requete
+        $query  = "select * from users where nom='gerald';";     
+        $key = "pw";
+
+        print( $query );
+
+        $res = $mysqli->query( $query );
+        //print_r( $res );
+        if ( $res->num_rows == 0 )
+        {
+            print( "aucune données<br>");
+        }
+        else
+        if ( $res->num_rows == 1 )
+        {
+            print( "reponse sur une seule ligne<br>");
+            $ligne = $res->fetch_assoc();
+            $value  = $ligne[ $key ];
+            print( "value=$value<br>"); 
+        }
+        else
+        {
+            print( "reponse dans un tableau<br>");
+            while(    $ligne = $res->fetch_assoc()) 
+            { 
+                $value  = $ligne[ $key ];
+                print( "value=$value<br>"); 
+            }
+        }
+        $mysqli->close();
+?>
+
