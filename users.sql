8bd7bf4aada70bcf064c1cbbca5953c6c348b6f4
xavier <xavier.bourget@foreach-academy.fr>
2021-11-30 15:39:02 +0100

    login

---
+++ users.sql
@@ -0,0 +1,16 @@
+```
+drop table if exists users;
+
+create table users (
+    id int PRIMARY KEY not null auto_increment,
+    nom  varchar(30),
+    pw varchar(256),
+    idDep int
+);
+
+insert into users ( nom, pw, idDep ) values( "Zorro", "zora", 22 );
+insert into users ( nom, pw, idDep ) values( "Ligonnes", "danger", 44 );
+insert into users ( nom, pw, idDep ) values( "Nicolas", "glouglou", 20 );
+
+```
+
