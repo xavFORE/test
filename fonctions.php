f88023ec7ff86381fea9fa25305a221b18008b70
carliolivier <olivier.carlier.foreach@hotmail.com>
2021-12-02 11:09:47 +0100

    2

--- fonctions.php
+++ fonctions.php
@@ -31,6 +31,26 @@ function comboBox(  $table, $query=""  )
     $mysqli->close();
     print( "</select>\n");
 }
+function comboBoxcolonne(  $colonne ,$table, $query=""  )
+{
+    GLOBAL $servername, $username, $password, $database;
+
+    print( "<select name='$colonne'>\n");
+    $mysqli = new mysqli($servername, $username, $password, $database);
+    if ( $query == "")
+        $query  = "select $colonne as nom from $table;";
+    //print( $query."<br>");
+    $res = $mysqli->query( $query );
+    while(  ($ligne = $res->fetch_assoc()) )
+    {
+        $id  = $ligne[ 'id' ];
+        $nom = $ligne[ 'nom' ];
+        print( "<option value=$id> $nom</option>\n" );
+    }
+    $mysqli->close();
+    print( "</select>\n");
+}
+
 
 
 ?>
