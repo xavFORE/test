ddf6402132d6538b9e71c5568510c6e22c9b4c8c
unknown <hanane.alaoui.foreach@gmail.com>
2021-12-09 10:40:11 +0100

    ttf

---
+++ donneEmprunteursA.php
@@ -0,0 +1,13 @@
+<?php
+require_once "ressources.php";
+require_once "fonctions.php";
+
+
+        $query  = "select * from emprunteurs;";
+        //print( $query );
+        $res = query( $query );
+        $ligne = $res->fetch_assoc(); 
+        $nom= $ligne['nom']; 
+        $datedeNaiss= $ligne['dateNaiss']; 
+        print( $nom."<br>". $datedeNaiss);
+?>
---
+++ listEmprunteursH.php
@@ -0,0 +1,44 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+    <style>
+        div 
+        {
+            color:red;
+            font-size: 30px;
+        }
+    </style>
+</head>
+
+<body>
+<script>
+    function chercherDataBackB1()
+    {
+        const xhttp = new XMLHttpRequest();
+        
+        xhttp.onload = function() {
+                document.getElementById("affiche1").innerHTML =
+                this.responseText;
+        }
+
+        xhttp.open("GET", "donneEmprunteursA.php");
+        xhttp.send();
+    }
+
+
+</script>
+
+    <button onclick="chercherDataBackB1()">GO</button>
+    <br>
+    <div id="affiche1"></div>
+
+
+
+
+
+</body>
+</html>
