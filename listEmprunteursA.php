9a42014ce12e114eeb152fd9938c22c3fe17964a
xavier <xavier.bourget@foreach-academy.fr>
2021-12-09 11:11:37 +0100

    ajax6

--- listEmprunteursA.php
+++ listEmprunteursA.php
@@ -6,11 +6,18 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Document</title>
     <style>
-        div 
+        .aff 
         {
             color:red;
             font-size: 30px;
             height: 40px;
+            background-color: gray;
+            border: 2px solid red;
+        }
+        .list 
+        {
+            color:red;
+            font-size: 10px;
             background-color: gray;
             border: 2px solid red;
         }
@@ -23,12 +30,15 @@
     {
         const xhttp = new XMLHttpRequest();
         
-        xhttp.onload = function() {
-        document.getElementById("affiche1").innerHTML =
-                this.responseText;
+        xhttp.onload = function() 
+        {
+            console.log(this.responseText );
+            let dict =  JSON.parse(  this.responseText );
+            document.getElementById("affiche1").innerHTML = dict[ 'nom' ];
+            document.getElementById("affiche2").innerHTML = dict[ 'dateNaiss' ];
         }
 
-        xhttp.open("GET", "donneEmprunteurs.php");
+        xhttp.open("GET", "donneEmprunteursList.php");
         xhttp.send();
     }
 
@@ -38,8 +48,9 @@
 
     <button onclick="chercherDataBackB1()">GO</button>
     <br>
-    <div id="affiche1">nom</div>
-    <div id="affiche2">date</div>
+    <div class="aff" id="affiche1">nom</div>
+    <div class="aff" id="affiche2">date</div>
+    <div  class="list" id="list">list</div>
 
 
 
