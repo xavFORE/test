29e6c5ce6c2440c1b77ce292cbe33aec6efaab45
carliolivier <93927234+carliolivier@users.noreply.github.com>
2021-12-09 15:26:09 +0100

    maj

---
+++ addLivreoc.php
@@ -0,0 +1,11 @@
+<?php
+require_once "ressources.php";
+require_once "fonctions.php";
+     
+        $query  = "INSERT INTO livres (nom) VALUES ('nom');";
+        //print( $query );
+        query( $query );
+   
+     
+   
+?>
