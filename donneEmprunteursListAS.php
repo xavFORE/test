930d91f6bfa8522702469b39f96acfda842315eb
Santrain <alexis.santrain@orange.fr>
2021-12-09 11:56:32 +0100

    hr

---
+++ donneEmprunteursListAS.php
@@ -0,0 +1,17 @@
+<?php
+require_once "ressources.php";
+require_once "fonctions.php";
+
+
+        $query  = "select * from emprunteurs;";
+        //print( $query );
+        $res = query( $query );
+        $tab = [];
+        $ligne = $res->fetch_assoc();
+        while ($ligne = $res->fetch_assoc()) {
+            $tab[] = $ligne;
+        }
+         
+        $data = json_encode( $tab );
+        print( $data );
+?>
