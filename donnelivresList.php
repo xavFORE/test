71250056c28065547e8bf8e448edbf90c5412c0d
Nelly <nelly.descamps.foreach@gmail.com>
2021-12-09 14:32:23 +0100

    ajout list

---
+++ donnelivresList.php
@@ -0,0 +1,22 @@
+<?php
+require_once "ressources.php";
+require_once "fonctions.php";
+
+
+        $query  = "select * from livres;";
+        //print( $query );
+        $res = query( $query );
+        
+        //print_r($res );
+
+        $tab = [];
+        while ( $ligne = $res->fetch_assoc())
+        {
+        $ligne['nom'] = utf8_encode($ligne['nom']);
+        $tab[] = $ligne;
+        //print_r($tab)
+
+        }
+        print(json_encode($tab));
+       
+?>
