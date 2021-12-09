8849fb2ab6a3a1bc99ad144602a2cd9778ceaef6
Nelly <nelly.descamps.foreach@gmail.com>
2021-12-09 15:26:01 +0100

    maj

--- donnelivresList.php
+++ donnelivresList.php
@@ -2,21 +2,15 @@
 require_once "ressources.php";
 require_once "fonctions.php";
 
-
         $query  = "select * from livres;";
         //print( $query );
         $res = query( $query );
-        
-        //print_r($res );
 
         $tab = [];
         while ( $ligne = $res->fetch_assoc())
         {
-        $ligne['nom'] = utf8_encode($ligne['nom']);
-        $tab[] = $ligne;
-        //print_r($tab)
-
+            $ligne['nom'] = utf8_encode($ligne[ 'nom']);
+            $tab[] = $ligne;
         }
-        print(json_encode($tab));
-       
+        print( json_encode( $tab ) );
 ?>
