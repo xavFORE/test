62fc55319d264f8ff4ca6b985a5cf38d9e6a6a58
carliolivier <93927234+carliolivier@users.noreply.github.com>
2021-12-09 16:25:55 +0100

    maj

---
+++ addLivreoc.php
@@ -0,0 +1,10 @@
+<?php
+    require_once "ressources.php";
+    require_once "fonctions.php";
+
+    $titre = $_GET[ 'titre'];
+    $auteur = $_GET[ 'auteur'];
+    $query  = "insert into livres ( nom, auteur ) values ('$titre','$auteur');";
+    //print( $query );
+    query( $query );
+?>
