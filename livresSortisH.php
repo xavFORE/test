e2d9288d705aa5a45264e11b8b104d3cb72d692f
unknown <hanane.alaoui.foreach@gmail.com>
2021-12-10 16:34:23 +0100

    aar1

---
+++ livresSortisH.php
@@ -0,0 +1,24 @@
+<?php
+    require_once "ressources.php";
+    require_once "fonctions.php";
+
+    $query  = "select * from livrsSortis;";
+    //print( $query );
+    $res = query( $query );
+
+    $tableauData = [];
+    while ( $ligne = $res->fetch_assoc())
+    {
+        $ligne['nom']=utf8_encode($ligne['nom']);
+        $tableauData[] = $ligne;
+    } 
+    //print_r($tableauData);
+    print( json_encode( $tableauData ) );
+    /*
+    back -> 
+    tableau brute (json_encode)-> text 
+    -> front 
+    -> (JSON.parse)
+    -> tableau brute
+    */
+?>
