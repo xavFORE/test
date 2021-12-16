66718875308e4fdf86b25e7e5fd749f93fed5247
xavier <xavier.bourget@foreach-academy.fr>
2021-12-16 11:21:29 +0100

    init nettoie

--- addLivre.php
+++
@@ -1,9 +0,0 @@
-<?php
-    require_once "ressources.php";
-    require_once "fonctions.php";
-
-    $titre = $_GET[ 'titre'];
-    $query  = "insert into livres ( nom ) values ('$titre');";
-    //print( $query );
-    query( $query );
-?>
