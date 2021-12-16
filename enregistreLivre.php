7933fe4fe8d376352bd4c1eda97d727632cb5601
xavier <xavier.bourget@foreach-academy.fr>
2021-12-16 11:22:30 +0100

    init 2

--- enregistreLivre.php
+++
@@ -1,8 +0,0 @@
-<?php
-    // enregistreLivre.php?nom=martine à la plage
-    require_once "ressources.php";
-    require_once "fonctions.php";
-    $nom = $_GET[ 'nom' ];
-    $nom = urldecode( $nom );
-    query( "insert into livres (nom) values ( '$nom' );" );
-?>
--- listAuteurs.php
+++
@@ -1,5 +0,0 @@
-<?php
-    require_once "ressources.php";
-    require_once "fonctions.php";
-    query2json( "select * from auteurs;" );
-?>
