6662462602a797b5086cfbf30fb3a40993a9aeff
xavier <xavier.bourget@foreach-academy.fr>
2021-12-28 17:21:29 +0100

    revision calcul

---
+++ calcul.php
@@ -0,0 +1,18 @@
+<?php
+    $val1       = $_POST[ 'v1' ];
+    $val2       = $_POST[ 'v2' ];
+    $operator   = $_POST[ 'op' ];
+    
+    /*
+    $val1       = '12';
+    $val2       = '5';
+    $operator   = "+";
+    */
+    //print( "$val1  $operator  $val2" );
+    $resultat = 0;
+    if ( $operator   == "add" )
+        $resultat = $val1 + $val2; 
+    elseif ( $operator   == "sous" )
+        $resultat = $val1 - $val2; 
+    print( $resultat ); 
+?>
