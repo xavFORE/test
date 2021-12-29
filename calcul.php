1f9440b180ed868cad49e4b2c2b5e792ba282168
karimm <karimm991@gmail.com>
2021-12-29 10:52:46 +0100

    getData2

--- calcul.php
+++ calcul.php
@@ -1,22 +1,34 @@
 <?php
+
+
     $val1       = $_POST[ 'v1' ];
     $val2       = $_POST[ 'v2' ];
     $operator   = $_POST[ 'op' ];
+
+
     
-    /*
+   /* 
     $val1       = '12';
-    $val2       = '5';
-    $operator   = "+";
-    */
+    $val2       = '0';
+    $operator   = "div";    */
+    
     //print( "$val1  $operator  $val2" );
     $resultat = 0;
     if ( $operator   == "add" )
         $resultat = $val1 + $val2; 
     elseif ( $operator   == "sous" )
-        $resultat = $val1 - $val2; 
-   
-    else ( $operator == "mult" )
+        $resultat = $val1 - $val2;    
+    elseif ( $operator == "mult" )
         $resultat = $val1 * $val2;
+    elseif ( $operator == "div"  )
+    {
+        if ($val2 != 0) {
+            $resultat = $val1 / $val2;
+        } else {
+            $resultat = "divison impossible";
+        }
+    }
+    else
+        $resultat = "opérateur non valide.";
 
      print( $resultat );
-?>
