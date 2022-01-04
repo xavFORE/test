2f0de6ea38ecc4739796474ae18dfea78988cabf
unknown <hanane.alaoui.foreach@gmail.com>
2022-01-04 10:11:56 +0100

    ttt

--- session1WC.php
+++ session1WC.php
@@ -1,5 +1,10 @@
 <?php
+ini_set("session.use_cookies", 0);
+ini_set("session.use_only_cookies", 0);
+ini_set("session.use_trans_sid", 1);
+ini_set("session.cache_limiter", "");
 session_start();
+//dectionaire
 $_SESSION[ 'plat'] = "couscous";
 $_SESSION[ 'boisson'] = "sidi brahim";
 ?>
