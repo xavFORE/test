ae1e6eb852d9e0029a736d6dbede4d6c41bd1f61
xavier <xavier.bourget@foreach-academy.fr>
2022-01-04 15:06:57 +0100

    authentificatin

--- mapage.php
+++ mapage.php
@@ -1,3 +1,15 @@
+<?php
+session_start();
+
+if ( !isset( $_SESSION[ 'nom' ]) )
+{
+    session_destroy();
+    header( "location: questions1.php");
+}
+
+$nom = $_SESSION[ 'nom' ];
+
+?>
 <!DOCTYPE html>
 <html lang="en">
 <head>
@@ -10,18 +22,25 @@
         {
             background-image : url( "images/av4.png");
         }
+        .pop
+        {
+            z-index : 10;
+            width : 300px; 
+            height : 300px;
+            background-color : grey; 
+            border: 2px solid black; 
+            margin: auto;
+        }
     </style>
 
 </head>
 <body >
     <div>
-    ma super belle page <br>
+    ma super belle page de <?=$nom?><br>
+    <br>
     <a href="session2WC.php">suite</a>
     </div>
 </body>
 </html>
 <?php
-$_SESSION[ 'plat']      = "couscous";
-$_SESSION[ 'boisson']   = "sidi brahim";
 ?>
-
