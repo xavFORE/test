9a6e205bd706d7b41358c20df4291c182bda350b
bouchra <bouchra.saillier.foreach@gmail.com>
2022-01-04 09:23:28 +0100

    mardi

--- session2WC.php
+++ session2WC.php
@@ -1,4 +1,9 @@
 <?php
+if ($_GET && $_GET ['PHPSESSID'])
+{
+ $sessionID = $_GET ['PHPSESSID'];
+  session_id ($sessionID);
+}
 session_start();
 $plat       = $_SESSION[ 'plat'];
 $boisson    = $_SESSION[ 'boisson'];
