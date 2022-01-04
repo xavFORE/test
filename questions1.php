9420e91381a61ff71eb661a7a36b81354fe69e4f
remi <remi.verhaeghe.foreach@gmail.com>
2022-01-04 15:55:09 +0100

    modif

--- questions1.php
+++ questions1.php
@@ -1,32 +1,31 @@
 <?php
-    if ( $_POST )
+
+if ( $_POST )
+{
+    if (  isset($_POST[ 'cancel']) )
     {
-        if (  isset($_POST[ 'cancel']) )
-        {
-            ini_set("session.use_cookies", 0);
-            ini_set("session.use_only_cookies", 0);
-            ini_set("session.use_trans_sid", 1);
-            ini_set("session.cache_limiter", "");
-        }
-        else
-            setcookie("miam", "miam");
-            
-        session_start();
-        //include( "mapage.php");
+        ini_set("session.use_cookies", 0);
+        ini_set("session.use_only_cookies", 0);
+        ini_set("session.use_trans_sid", 1);
+        ini_set("session.cache_limiter", "");
+    
+        
     }
-    else
-    {
-        if( !isset($_COOKIE['miam']) )
-        {
-    ?>
-        <div class="pop">
-            veuillez accepter les cookies
-            <form action="#" method="POST">
-                <button type="submit" name="ok">OK</button>
-                <button type="cancel" name="cancel">NO</button>
-            </form>
-        </div>
-    <?php
-        }
-    }
-    ?>
+    session_start();
+    include( "mapage.php");
+}
+else
+{
+    include( "mapage.php");
+?>
+    
+    <div class="pop">
+        veuillez accepter les cookies
+        <form action="#" method="POST">
+            <button type="submit" name="ok">OK</button>
+            <button type="cancel" name="cancel">NO</button>
+        </form>
+    </div>
+<?php
+}
+?>
