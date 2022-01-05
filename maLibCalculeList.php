97c7e004b9fd998fc3c19971f15c068a3adcb94e
carliolivier <93927234+carliolivier@users.noreply.github.com>
2022-01-05 15:59:39 +0100

    maj

---
+++ maLibCalculeList.php
@@ -0,0 +1,33 @@
+<?php
+
+    function sommeDuneListe( $liste )
+    {
+        if ( count($liste) == 0 )
+        {
+            throw new Exception( "la liste est vide" );
+        }
+
+        $total = 0;
+        foreach ($liste as $key => $value)
+            if ( gettype( $value) == "integer" || gettype( $value) == "double") 
+                $total += $value;
+            else
+                throw new Exception( "$value n'est pas un chiffre" );                
+        
+       
+        return $total;
+    }
+
+
+    function moyenne( $liste)
+    {
+        $total = sommeDuneListe( $liste );
+
+        if ( count($liste) == 0 )
+        {
+            throw new Exception( "division par zéro" );
+        }
+        $moyenne = $total / count($liste);
+        return $moyenne;
+    }
+?>
