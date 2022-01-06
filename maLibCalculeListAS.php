5071697e57ecc01a9a7405a12cdca23ec9b67281
Santrain <alexis.santrain@orange.fr>
2022-01-06 09:57:32 +0100

    ji

---
+++ maLibCalculeListAS.php
@@ -0,0 +1,30 @@
+<?php
+    function sommeDuneListe( $liste )
+    {
+        if ( count($liste) == 0 )
+        {
+            throw new Exception( "la liste est vide" );
+        }
+
+        $total = 0;
+        foreach ($liste as $key => $value)
+            $type = gettype( $value);
+            if ( $type == "integer" || $type == "double") 
+                $total += $value;
+            else
+                throw new Exception( "$type n'est pas un chiffre" );                
+        return $total;
+    }
+
+
+    function moyenne( $liste)
+    {
+        $total = sommeDuneListe( $liste );
+
+        if ( count($liste) == 0 )
+        {
+            throw new Exception( "division par zéro" );
+        }
+        $moyenne = $total / count($liste);
+        return $moyenne;
+    }
---
+++ testTryCatchAS.php
@@ -0,0 +1,23 @@
+<?php
+    require( "maLibCalculeListAS.php" );
+
+    try 
+    {
+
+        //$maListe = [ 3, 12, 2, 8 ];
+        //$maListe = [ 3, True, 2, 8 ];
+        $maListe = [ 3, 12, 'tata', 2, 8 ];
+        $maListe = [  2 , 3.2, 2  ];
+        $maListe = [  2 , [2, 2]  ];
+        //$maListe = [  0 , 0, 0  ];
+        $maListe = [  2 , -2  ];
+        $maListe = [  '2' , '-2'  ];
+
+        $res = moyenne( $maListe );
+        
+        print( $res );
+    } 
+    catch ( Exception $th) 
+    {
+        print( "une erreur s'est produite : " .$th->getMessage() );
+    }
