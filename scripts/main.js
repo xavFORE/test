a00ba0b8b546efc3649bad948c44d156784a124e
karimm <karimm991@gmail.com>
2022-01-10 01:07:46 +0100

    ok

--- scripts/main.js
+++ scripts/main.js
@@ -1,3 +1,7 @@
+import tabJoursEnOrdre from './Utilitaire/gestionTemps.js';
+
+console.log("Depuis MAIN JS:"+tabJoursEnOrdre);
+
 const CLEFAPI = 'ea9026527c620ff4e5118a5db3b9d5cb';
 let resultatsAPI;
 
