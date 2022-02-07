d30f0877066a28d4abcb99d070ae764712727456
xavier <xavier.bourget@foreach-academy.fr>
2022-02-07 12:16:51 +0100

    TEST: mailer

--- src/Controller/ContactController.php
+++ src/Controller/ContactController.php
@@ -5,9 +5,15 @@ namespace App\Controller;
 use App\Entity\Contact;
 use App\Form\ContactType;
 
+
+
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
+
+use Symfony\Component\Mailer\MailerInterface;
+use Symfony\Component\Mime\Email;
 
 class ContactController extends AbstractController
 {
@@ -21,11 +27,43 @@ class ContactController extends AbstractController
         ]);
     }
 
-    public function contact(): Response
+    public function contact( Request $request, MailerInterface $mailer ): Response
     {
         $contact = new Contact();
         $form = $this->createForm( ContactType::class, $contact );
 
+        $form->handleRequest($request);
+        if ( $form->isSubmitted() && $form->isValid() ) 
+        {
+            // expedition du mail
+            $dep = $contact->getDepartement();
+            $name = $contact->getName();
+            $mail = $dep->getMail();
+            
+            
+
+            $email = (new Email())
+                        ->from( 'hello@example.com')
+                        ->to( $mail )
+                        //->cc('cc@example.com')
+                        //->bcc('bcc@example.com')
+                        //->replyTo('fabien@example.com')
+                        //->priority(Email::PRIORITY_HIGH)
+                        ->subject("un message de $name ")
+                        ->text('Sending emails is fun again!')
+                        ->html('<p>See Twig integration for better HTML integration!</p>');
+            
+            $mailer->send($email);
+        
+
+            // sauvegarde du contact            
+            $doctrine = $this->getDoctrine();
+            $entityManager = $doctrine->getManager();
+            $entityManager->persist($contact); 
+            $entityManager->flush();            
+
+            return new Response( "<h1>formulaire OK<br>mail envoyé à $mail</h1>");
+        }
         return $this->render('contact/formulaire.html.twig', [
             'formulaire' => $form->createView(),
         ]);
